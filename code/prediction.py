344e625461816cd9a0bcd37341fdb3cd9860ee94
HDSpary <jdbde@outlook.com>
2018-07-09 20:08:17 +0800

    process output

--- code/prediction.py
+++ code/prediction.py
@@ -12,23 +12,36 @@ X = train_data.drop(['SalePrice'], axis=1)
 
 # endcoded the data
 encoded_training_predictors = pd.get_dummies(X)
+encoded_testing_predictors = pd.get_dummies(test_data)
+final_train, final_test = encoded_training_predictors.align(
+        encoded_testing_predictors, join='left', axis=1)
 
 # split the data set
-train_X, test_X, train_y , test_y = 
-    train_test_split(encoded_training_predictors, y, random_state=17)
+train_X, test_X, train_y , test_y = train_test_split(
+        final_train, y, random_state=17)
 
 # Bachup the data and fix the missing value
 imputed_X_train_plus = train_X.copy()
 imputed_X_test_plus = test_X.copy()
+imputed_final_test = final_test.copy()
+
 cols_with_missing = (col for col in train_X.columns if train_X[col].isnull().any())
+cols_with_missing_at_ouput = (col for col in final_test.columns if final_test[col].isnull().any())
 for col in cols_with_missing:
     imputed_X_train_plus[col + '_was_missing'] = imputed_X_train_plus[col].isnull()
     imputed_X_test_plus[col + '_was_missing'] = imputed_X_test_plus[col].isnull()
+    imputed_final_test[col + '_was_missing'] = imputed_final_test[col].isnull()
+
+for col in cols_with_missing_at_ouput:
+    imputed_X_train_plus[col + '_was_missing'] = imputed_X_train_plus[col].isnull()
+    imputed_X_test_plus[col + '_was_missing'] = imputed_X_test_plus[col].isnull()
+    imputed_final_test[col + '_was_missing'] = imputed_final_test[col].isnull()
 
 # Imputation
 imputer = Imputer()
 train_X = imputer.fit_transform(imputed_X_train_plus)
 test_X = imputer.transform(imputed_X_test_plus)
+output_test = imputer.transform(imputed_final_test)
 
 # train the model
 model = XGBRegressor(n_estimators=1000, learning_rate=0.05)
@@ -37,3 +50,8 @@ model.fit(train_X, train_y, early_stopping_rounds=5, eval_set=[(test_X, test_y)]
 # test the accuary
 val_predictions = model.predict(test_X)
 print("Mean Absolute Error : " + str(mean_absolute_error(val_predictions, test_y)))
+
+# output
+ouput_predictions = model.predict(output_test)
+print(ouput_predictions)
+
