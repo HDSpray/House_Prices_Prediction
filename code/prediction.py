ce98d27d2d921435003c617938d02fb97f9b0ad9
HDSpary <jdbde@outlook.com>
2018-07-09 17:12:29 +0800

    add encoded

--- code/prediction.py
+++ code/prediction.py
@@ -10,13 +10,25 @@ test_data = pd.read_csv('../input/test.csv')
 y = train_data.SalePrice
 X = train_data.drop(['SalePrice'], axis=1)
 
-# split the data set
-train_X, test_X, train_y , test_y = train_test_split(X, y, random_state=17)
+# endcoded the data
+encoded_training_predictors = pd.get_dummies(X)
 
-# resolve missing value
+# split the data set
+train_X, test_X, train_y , test_y = 
+    train_test_split(encoded_training_predictors, y, random_state=17)
+
+# Bachup the data and fix the missing value
+imputed_X_train_plus = train_X.copy()
+imputed_X_test_plus = test_X.copy()
+cols_with_missing = (col for col in train_X.columns if train_X[col].isnull().any())
+for col in cols_with_missing:
+    imputed_X_train_plus[col + '_was_missing'] = imputed_X_train_plus[col].isnull()
+    imputed_X_test_plus[col + '_was_missing'] = imputed_X_test_plus[col].isnull()
+
+# Imputation
 imputer = Imputer()
-train_X = imputer.fit_transform(train_X)
-test_X = imputer.transform(test_X)
+train_X = imputer.fit_transform(imputed_X_train_plus)
+test_X = imputer.transform(imputed_X_test_plus)
 
 # train the model
 model = XGBRegressor(n_estimators=1000, learning_rate=0.05)
